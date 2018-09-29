dbe7f2dfa9fda4e29a014263b09179f173ca1f6a
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 06:56:48 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -29,3 +29,26 @@ class Board
   def full? 
     cells.all? {|index| index == "X" || index == "O"}
   end 
+  def turn_count #Returns the amount of turns
+    cells.count {|cell| cell == "X" || cell == "O"}
+  end
+
+  def taken?(num) #Returns false if the position is empty
+    num = num.to_i - 1 if num.class == String
+    !(cells[num].nil? || cells[num] == " ")
+  end
+
+  def valid_move?(position) # returns true for 1-9 that is not taken
+    new_position = input_to_index(position)
+    new_position.between?(0,8) && !taken?(new_position)
+  end
+
+  def update(position, player)
+    index = input_to_index(position)
+    cells[index] = player.token
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+end
