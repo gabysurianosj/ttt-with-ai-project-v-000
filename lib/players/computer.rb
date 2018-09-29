be081e2d0498d4a9968166761f56fad514f261ed
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 07:17:04 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -11,4 +11,20 @@ module Players
   class Computer < Player 
     attr_reader :board 
     WIN_COMBINATIONS= [
-      ]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
+    
+    def move(board)
+      @board = board 
+      
+      opponent_token 
+      
+      if check_for_winning_move != nil 
+        number 
