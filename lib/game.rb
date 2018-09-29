6dc4741df4a37399ea683d915e883c5f2cf1501f
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 07:10:14 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,34 @@
 class Game 
   attr_accessor :board, :player_1, :player_2 
   
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  def initialize(p1 = Players::Human.new("X"), p2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = p1
+    @player_2 = p2
+    @board = board
+  end
+
+  def board
+    @board
+  end
+
+  def current_player
+    board.turn_count.even? ? @player_1 : @player_2
+  end
+
+  def over?
+    won? || draw? ? true : false
+  end
+  
+  
 end 
